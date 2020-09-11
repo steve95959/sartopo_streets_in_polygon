b5d93e034d5c5021ffca6a8a1835191c0d1a7945
Tom Grundy <caver456@gmail.com>
2020-09-11 11:10:44 -0700

    devel
    # 9-11-20     TMG        handles multi-segment roads, but needs improvement;
    #                          splits roads at zone edges; creates assignments
    #                          using queued bulk-import

--- sartopo_streets_in_polygon.py
+++ sartopo_streets_in_polygon.py
@@ -38,22 +38,29 @@
 #  9-6-20     TMG        working for lines obtained from map using API
 #  9-7-20     TMG        working for buffer assignments, using kml from QGIS
 #                          to do: account for multi-segment roads in kml
+# 9-11-20     TMG        handles multi-segment roads, but needs improvement;
+#                          improved chopping at zone edges; creates assignments
+#                          using queued bulk-import
 #-----------------------------------------------------------------------------
 
 from sartopo_python import SartopoSession
 import json
 from shapely.geometry import LineString,Polygon
+from shapely.ops import split
 # from pykml import parser
 # from lxml import etree
 import xml.etree.ElementTree as et
+import copy
 
 
-mapID="QARV"
-boundaryName="e4"
+# mapID="QARV"
+mapID="9B0"
+boundaryName="evac"
 boundaryCoords=None
 streets={}
 
-sts=SartopoSession("sartopo.com",mapID,configpath="C:\\Users\\caver\\Downloads\\sts.ini",account="caver456@gmail.com")
+# sts=SartopoSession("sartopo.com",mapID,configpath="C:\\Users\\caver\\Downloads\\sts.ini",account="caver456@gmail.com")
+sts=SartopoSession(mapID=mapID)
 
 shapes=sts.getFeatures("Shape")
 print(str(len(shapes))+" shapes found.")
@@ -76,25 +83,114 @@ print(str(len(shapes))+" shapes found.")
 #         for point in segment.points:
 #             streets[name].append([point.latitude,point.longitude]) # prelim: assume there's only one segment
 
-# this kml parsing works for QGIS KML export and is not guaranteed to work for export from other tools;
+# this kml parsing works for KML export from QGIS or from SARTopo and is not guaranteed to work for export from other tools;
 #  see the detailed parsing documentation at https://docs.python.org/3.8/library/xml.etree.elementtree.html
 #  and look at the kml file itself to see what is happening
 kmlFile='C:\\Users\\caver\\Documents\\nevCoRoads.kml'
+# kmlFile='C:\\Users\\caver\\Downloads\\apiTest1.kml'
+# kmlFile='C:\\Users\\caver\\Downloads\\joinTest4.kml'
 kml=et.parse(kmlFile)
 root=kml.getroot()
 ns={'kml':root.tag.split('}')[0][1:]} # determine the namespace from the kml kmlns 'non-attribute'
 placemarks=root.findall('.//kml:Placemark',ns)
 unnamedIndex=1
+# merging multiple segments of the same street:
+#  the GIS data will likely be broken into 'segments' between intersections.  Each
+#  segment is its own placemark, and they do not necessarily appear in the same sequence
+#  or even in the same direction in the kml file as they should in the sartopo map.
+# Therefore, the next placemark parsed with the same name as an existing placemark
+#  is not necessarily contiguous with the already-existing placemark.
+# This parser should be able to deal with that data.
+# 1. When reading a placemark, if there is already a placemark by the same name,
+#  save this new placemark with an indexed name i.e. 'MyStreet_2'
+# 1. determine if the start or end coords if the new placemark are 'near' the start or
+#  end coordinates of the existing placemark.  If so, append all but the first point
+#  of the new placemark to the existing placemark (after reversing the 
+#  order of coords of the new placemark if necessary, i.e. if end of new placemark is
+#  'close' to end of existing placemark).  If not, make a new placemark suffixed with
+#  an index number 'MyRoad_1' or such, to be post-processed later.
+# 2. For post-processing, recursively repeat until all suffixed placemarks are processed:
+#  for each suffixed placemark, determine if the start or end points are 'close to'
+#  the start or end points of the first placemark; if it's a match, append as above
+#  (after flipping as above if necessary,i.e. end of new placemark is 'close to' end
+#  of first placemark) then delete the suffixed placemark from the list of suffixed
+#  placemarks to process.
 for pm in placemarks:
     try:
         name=pm.find(".//kml:SimpleData[@name='FULLNAME']",ns).text
     except:
-        name="UNNAMED_"+str(unnamedIndex)
-        unnamedIndex+=1
+        try:
+            name=pm.find('.//kml:name',ns).text
+        except:
+            name="UNNAMED_"+str(unnamedIndex)
+            unnamedIndex+=1
     # print("  processing "+name)
     coordinates=pm.find('.//kml:coordinates',ns)
-    streets[name]=[list(map(float,x.split(','))) for x in coordinates.text.split()]
+    # add [0:2] to get rid of z coord in case it exists
+    streets.setdefault(name,[]).append([list(map(float,x.split(',')[0:2])) for x in coordinates.text.split()])
 print("Done parsing kml.  "+str(len(streets))+" streets were read.")
+
+# reduce multi-segment streets by joining end-to-end where possible
+# to do 9-10-20:
+#   - if no matches, try reversing the first segment then doing the loop again;
+#       if still no matches, then add it as a separate street
+#   - investigate names with many suffixes
+#   - investigate odd shapes e.g. Brunswick Road:131:1165:1517:1675:1740:1770:1791:1807
+d=0.0001 # tolerance to detect adjacent segment endpoints - about 30 feet
+unmatchedIndex=0
+streetNames=list(streets.keys()) # this list may be added to at the end of each iteration
+for streetName in streetNames:
+    segments=streets[streetName]
+    reduced=segments[0]
+    segments=segments[1:] # this works even if segments only had one element
+    # print("STREET:"+streetName)
+    # print("FIRST:"+str(reduced))
+    # print("REST:"+str(segments))
+    while len(segments)>0:
+        # print("  "+str(reduced))
+        [lastReducedLat,lastReducedLon]=reduced[-1]
+        # print("  lastReducedLat="+str(lastReducedLat)+"  lastReducedLon="+str(lastReducedLon))
+        found=False
+        for segment in copy.copy(segments): # copy, so that segments.remove calls don't interfere with this loop
+            firstSegmentPoint=segment[0]
+            lastSegmentPoint=segment[-1]
+            [firstSegmentLat,firstSegmentLon]=firstSegmentPoint
+            [lastSegmentLat,lastSegmentLon]=lastSegmentPoint
+            # print("    "+str(firstSegmentPoint)+"  -->  "+str(lastSegmentPoint))
+            if abs(firstSegmentLat-lastReducedLat)<d and abs(firstSegmentLon-lastReducedLon)<d:
+                # print("      match, in same order")
+                # segment is adjacent and in same sequence as reduced: append (all but first point) then remove
+                found=True
+                reduced.extend(segment[1:])
+                segments.remove(segment)
+            elif abs(lastSegmentLat-lastReducedLat)<d and abs(lastSegmentLon-lastReducedLon)<d:
+                # print("      match, needs reversing")
+                # segment is adjacent but reversed: reverse, append (all but first point), then remove
+                found=True
+                reduced.extend(segment[-2::-1]) # this gets a reversed copy of the list, without the end element, without changing it
+                segments.remove(segment)
+            # else:
+            #     print("      no match")
+    # any segments remaining were not matched at all i.e. are not end-to-end adjacent with the
+    #  starting segment or segments that joined to it; but, they still may form a new group of
+    #  contiguous segments; so, save them as a new street and let them go through this processing
+    #  again on a future iteration of the while loop, which is effectively recursion
+        if not found:
+    # if len(segments)>0:
+            unmatchedIndex+=1
+            newStreetName=streetName+":"+str(unmatchedIndex)
+            # print("  NO MATCHES.  ADDING NEW STREET "+newStreetName)
+            streets[newStreetName]=segments
+            streetNames.append(newStreetName)
+            segments=[]
+
+    # now store the reduced street    
+    streets[streetName]=reduced
+    # print("FINALIZED "+streetName+": "+str(streets[streetName]))
+
+# print(json.dumps(streets,indent=3))
+
+# print("Eskaton Circle:\n"+str(streets["Eskaton Circle"]))
 
 # print(str(len(streets.keys()))+' streets read from '+gpx_file)
 
@@ -111,11 +207,24 @@ for shape in shapes:
 
 # 2. find streets at least partially covered by the boundary shape
 #   streets is a dictionary: key = street name, val = coordinate list
-streetsToAdd={}
+streetsToAdd1={}
 for streetName in streets.keys():
     coords=streets[streetName]
     if LineString(coords).intersects(boundaryPolygon):
-        streetsToAdd[streetName]=coords
+        streetsToAdd1[streetName]=coords
+
+# growing the boundary shape twice seems to give better results -
+#  use the first grown boundary to split the streets, then only
+#  select streets that are completely within the second grown boundary
+streetsToAdd={}
+grownBoundary1=boundaryPolygon.buffer(0.0003) # about 30 meters
+grownBoundary2=grownBoundary1.buffer(0.0003) # about 30 more meters
+for streetName in streetsToAdd1.keys():
+    coords=streets[streetName]
+    lines=split(LineString(coords),grownBoundary1)
+    for line in lines:
+        if line.within(grownBoundary2):
+            streetsToAdd[streetName]=line.coords
 
 # 3. send the results to sartopo
 
@@ -132,6 +241,7 @@ for folder in folders:
 if fid is None:
     fid=sts.addFolder('results')
 
+#  lump all objects into one large 'save' API request, using 'queue' argument
 print(str(len(streetsToAdd))+" streets are at least partially covered by "+boundaryName+":")
 for streetName in streetsToAdd.keys():
     # as lines:
@@ -139,9 +249,13 @@ for streetName in streetsToAdd.keys():
     # as line assignments:
     # result=sts.addLineAssignment(letter=streetName+".la",points=streetsToAdd[streetName],folderId=fid)
     # as buffer assignments:
-    result=sts.addAreaAssignment(letter=streetName,points=list(LineString(streetsToAdd[streetName]).buffer(0.0001).exterior.coords),folderId=fid)
+    result=sts.addAreaAssignment(letter=streetName,points=list(LineString(streetsToAdd[streetName]).buffer(0.0001).exterior.coords),folderId=fid,queue=True)
     print("  "+streetName+":"+str(result))
+sts.flush()
 
+# sts.addMarker(lat=39,lon=-120,title="a",queue=True)
+# sts.addMarker(lat=39.001,lon=-120.001,title="b",queue=True)
+# sts.flush()
 
 # three-vertex-centerline buffer test shape, entirely inside shape 'e3':
 # {"properties":{"title":"buf1","description":"","folderId":null,"gpstype":"TRACK","stroke-width":2,"stroke-opacity":1,"stroke":"#FF0000","pattern":"solid"},"geometry":{"coordinates":[[[-119.3368561590181,37.96112666548135],[-119.26419088810952,37.89948559193528],[-119.21205461142533,37.96117000752221],[-119.21203546684448,37.96118513325123],[-119.21201043790118,37.96119333133232],[-119.21198333502517,37.96119335368434],[-119.2119582843836,37.96118519690443],[-119.21193909970961,37.961170102785964],[-119.21192870169588,37.961150369268296],[-119.21192867334575,37.96112900059819],[-119.21193901897527,37.96110924996199],[-119.26412407756902,37.899367043360144],[-119.26414140714591,37.899352897377135],[-119.26416395072032,37.899344491555986],[-119.26418880362515,37.89934290896174],[-119.26421276364351,37.89934835350702],[-119.2642327436041,37.89936012367826],[-119.33701716743222,37.96110233608887],[-119.3370305399595,37.96111879947132],[-119.33703572610682,37.9611379171178],[-119.33703206500518,37.96115725287176],[-119.33702002318752,37.961174342785185],[-119.33700113513875,37.96118700909829],[-119.24224405506062,38.00448385991809],[-119.24221840596034,38.00449076034439],[-119.24219135800698,38.00448940127205],[-119.24216702900625,38.0044799896071],[-119.24214912282795,38.004463958187536],[-119.24214036552543,38.00444374764827],[-119.24214209031858,38.004422434858675],[-119.2421540346233,38.004403264498116],[-119.24217438002746,38.00438915508298],[-119.3368561590181,37.96112666548135]]],"type":"Polygon"}}
